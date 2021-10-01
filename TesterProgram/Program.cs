b996d52e7d8b5dc9e8752c22e066189c4bc89ca5
LynnDCoding@outlook.com <LynnDCoding@outlook.com>
2021-10-01 01:20:47 -0500

    Adding Vehicle

--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -28,16 +28,30 @@ namespace TesterProgram
             s3.LastName = "Weasley";
             s3.Id = "003";
             s3.Gpa = 2;
-            Console.WriteLine("Using the Constructor Method, the names of our students are\n");
+            Console.WriteLine("\nUsing the Constructor Method, the names of our students are");
             Console.WriteLine($"First Name: { s1.FirstName} Last Name: {s1.LastName} ID: {s1.Id}  GPA: {s1.Gpa}");
             Console.WriteLine($"First Name: { s2.FirstName} Last Name: {s2.LastName} ID: {s2.Id}  GPA: {s2.Gpa}");
             Console.WriteLine($"First Name: { s3.FirstName} Last Name: {s3.LastName} ID: {s3.Id}  GPA: {s3.Gpa}");
 
             //---------using the ToString()
-            Console.WriteLine("\nUsing the ToString Method, the names of our students are\n");
+            Console.WriteLine("\nUsing the ToString Method:");
             Console.WriteLine(s1);
             Console.WriteLine(s2);
             Console.WriteLine(s3);
+
+            //public Vehicle(string make, string model, int year, float weight)
+
+            Vehicle v1 = new Vehicle();         //CONSTRUCTOR METHOD
+            v1.Make = "Chevy";
+            v1.Model = "Impala";
+            v1.Year = 1968;
+            v1.Weight = 200;
+            Console.WriteLine("\nUsing the Constructor Method:");
+            Console.WriteLine($"Make: { v1.Make} Last Name: {v1.Model} ID: {v1.Year}  GPA: {v1.Weight}");
+
+            //---------using the ToString()
+            Console.WriteLine("\nUsing the ToString Method:");
+            Console.WriteLine(v1);
 
         }//end Main()
     }//end class
